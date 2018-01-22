f37ae0034b55a53bb8886343d609d1ba5f00b40b
admin <admin@DESKTOP-EKBA8NT>
2018-01-22 13:46:35 +0530

    First commit

---
+++ SeleniumGit/src/test/java/basic/MyFirstScript.java
@@ -0,0 +1,19 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class MyFirstScript {
+	
+	
+	@Test
+	public void test(){
+		System.setProperty("webdriver.gecko.driver", "D:\\Training\\TrainingContent\\SeleniumJars_Software\\geckodriver_64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		
+		System.out.println(" my first script");
+	}
+
+}
